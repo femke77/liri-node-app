ef909f8c967f6707f44e2a1b284c086307d02e3c
Meg Meyers <megan.g.meyers@gmail.com>
2019-12-15 16:18:32 -0800

    inquirer added and working for CLI user prompts

--- liri.js
+++ liri.js
@@ -3,7 +3,24 @@ require("dotenv").config();
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
-
+var inquirer = require("inquirer");
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "method",
+        message: "Please tell LIRI what you want to do.",
+        choices: ["concert-this", "spotify-this", "movie-this", "do-what-it-says"],
+    },
+    {
+        type: "input",
+        name: "userInput",
+        message: "Enter the artist, band name, song, or movie according to the LIRI command you chose previously."
+        
+    }
+]).then(function (response) {
+    console.log(`${response.method} and ${response.userInput}`)
+});
